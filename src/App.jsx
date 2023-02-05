e38180665926239fee0f39dbd9c9503a9cfea0d8
samspuds <90201674+samspuds@users.noreply.github.com>
2023-02-05 15:19:21 +0000

    Build basic structure of the game

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
-
-import './App.css'
+import React from "react"
 
 function App() {
- 
-
-  return (
-    <div className="App">
-      
-    </div>
-  )
+       return(
+            <div className="App">
+                <h1></h1>
+                <textarea />
+                <h4></h4>
+                <button></button>
+                <h1></h1>
+            </div>
+        )
 }
 
 export default App
