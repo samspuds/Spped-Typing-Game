779c0c2f6cdff07ae8a561cb4f177dae4e414e92
samspuds <90201674+samspuds@users.noreply.github.com>
2023-02-05 21:08:56 +0000

    fixed bug
    empty space being counted as words in the array fixed with filter

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
       function wordCount(text){
         const wordArr = text.split(" ")
-        return wordArr.length
+        return wordArr.filter(word => word !== "").length
       }
 
        return(
