b15dca8ae96aebfe7be508df228bb708f989763e
samspuds <90201674+samspuds@users.noreply.github.com>
2023-02-05 20:40:59 +0000

    Track the state of  the text at each keystone
    Words inputted into the text area using state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from "react"
 
 function App() {
+      const[text, setText] = React.useState("")
+      function handleChange(e){
+        const{value, name} = e.target
+        setText(value)
+      }
        return(
             <div>
                 <h1>How fast do you type?</h1>
-                <textarea />
-                <h4>Time reminaing: ???</h4>
+                <textarea 
+                  type="text"
+                  name="text"
+                  value={text}
+                  onChange={handleChange}
+                />
+                <h4>Time remaining: ???</h4>
                 <button>Start</button>
                 <h1>Word count: ???</h1>
             </div>
